9bf797824942fb4be933d01c1c400f664e3eee55
Rafa Ramírez <raffe90@gmail.com>
2015-10-24 11:47:29 +0200

    Adds Database class

---
+++ database.rb
@@ -0,0 +1,11 @@
+require 'imdb'
+
+class Database
+	def initialize(db = 'movies.txt')
+		@db = db
+	end
+
+	def all
+		file_contents_array = IO.read(@db).split(/\r?\n/)
+	end
+end
--- pry.rb
+++ pry.rb
@@ -1,9 +1,9 @@
-print "[0]> "
+print "[0]>"
 counter = 0
 input = gets.chomp
 while input != 'exit!'
 	puts "=> #{eval input}"
 	counter+=1
-	print "[#{counter}]> "
+	puts "[#{counter}]>"
 	input = gets.chomp
 end
